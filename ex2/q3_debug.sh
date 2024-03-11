b3e7ff2f1a14d5980d1513c4651045b1b1b71b69
Leo <hao.yu.019@gmail.com>
2024-03-12 03:29:21 +0800

    --q3 debug script

---
+++ ex2/q3_debug.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+count=0
+output_file='q3.out'
+error_file='q3.err'
+test_file='q3_test.sh'
+
+#echo '$?=' "$?"
+
+until [[ "$?" -ne 0 ]]
+do
+ #echo '$?=' "$?"
+ ((count++))
+ bash ${test_file} 1>${output_file} 2>${error_file}
+done
+
+echo "the script has run ${count} times before it fails."
+echo "output:"
+cat ${output_file}
+echo -e "\nerror:"
+cat ${error_file}
