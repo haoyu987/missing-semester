25690651fcdd649765256ea9e5e824bff630b85e
Leo <hao.yu.019@gmail.com>
2024-03-12 15:55:23 +0800

    --q4

--- ex2/q3_debug.sh
+++ ex2/q3_debug.sh
@@ -5,17 +5,18 @@ output_file='q3.out'
 error_file='q3.err'
 test_file='q3_test.sh'
 
+./${test_file} 1>${output_file} 2>${error_file}
 #echo '$?=' "$?"
 
 until [[ "$?" -ne 0 ]]
 do
  #echo '$?=' "$?"
  ((count++))
- bash ${test_file} 1>${output_file} 2>${error_file}
+ bash ${test_file} 1>>${output_file} 2>>${error_file}
 done
 
 echo "the script has run ${count} times before it fails."
-echo "output:"
-cat ${output_file}
-echo -e "\nerror:"
-cat ${error_file}
+#echo "output:"
+#cat ${output_file}
+#echo -e "\nerror:"
+#cat ${error_file}
